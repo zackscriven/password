a465614c950d4645b7daf829715ba1cd8fac2b26
Zack Scriven <zackscriven@waterhammer.net>
2017-03-31 14:17:37 -0700

    a

--- bruteForcePasswordCracker.py
+++ bruteForcePasswordCracker.py
@@ -9,36 +9,49 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 """
+import time
 import string
-
-# function to convert a number n from base 10 to base b
-def numberToBase(n, b):
-	digits = []
-	while n:
-		digits.append(int(n % b))
-		n /= b
-	return digits[::-1]
-
-# variables
-characters = string.printable[:]
-base = len(characters)
-solved = False
+ 
+start = time.time()
+chars = list(string.printable)[:95]
+base = len(chars)
 n = 0
-maxAttempts = 999999
-password = input("Enter a password")
-
-
-while !solved and n < maxAttempts:
-	charList = numberToBase(n,base)
-	passwordGuess = ''
-	for x in charList:
-		passwordGuess = characters[x]
-	
-	
-	if passwordGuess == password:
-		solved = True
-		print('_______Statistics______')
-		print('Password: ' + passwordGuess)
-		print('Attempts: ' + str(n))
-	else:
-		n += 1
+password = '123456'
+solved = False
+ 
+if password == '':
+    print 'Your password is empty'
+    solved = True
+elif password == chars[n]:
+    print 'Your password is ' + chars[n]
+    solved = True
+else:
+    n += 1
+ 
+ 
+def numberToBase(n, b):
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n /= b
+    return digits[::-1]
+ 
+ 
+if not solved:
+    while n < 99999999:
+        lst = numberToBase(n, base)
+        word = ''
+        for x in lst:
+            word += str(chars[x])
+        # print word
+        if password == word:
+            solved = True
+            print '-Stats-'
+            print 'Pass: ' + word
+            print 'Attempts: ' + str(n)
+            print 'time: ' + str((time.time() - start)) + ' sec'
+            break
+        else:
+            n += 1
+if not solved:
+    print 'Unsolved after ' + str(n) + ' attempts!'
